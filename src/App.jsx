74aa84700192d44683f0fa312a2876af0b66ad19
Jamlie <jamlee977@outlook.com>
2025-05-20 19:31:22 +0300

    added integration with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import { withAuthenticator } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css";
@@ -9,7 +9,7 @@ import PropTypes from "prop-types";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-import  Home  from "./pages/Home";
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 
@@ -22,7 +22,10 @@ function App({ signOut, user }) {
             <main>
                 <Routes>
                     <Route path="/" element={<Home user={user} />} />
-                    <Route path="/dashboard" element={<Dashboard user={user} />} />
+                    <Route
+                        path="/dashboard"
+                        element={<Dashboard user={user} />}
+                    />
                     <Route path="/about" element={<About />} />
                 </Routes>
             </main>
@@ -36,5 +39,4 @@ App.propTypes = {
     user: PropTypes.object.isRequired,
 };
 
-//export default withAuthenticator(App);
-export default App;
+export default withAuthenticator(App);
